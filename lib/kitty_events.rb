24e2fbc35fe6e5bf1879390661514a0932be62a2
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2016-12-25 13:33:50 -0800

    Remove events class variable + Clean up register
    + since it's a module, it's `private_class_method`

--- lib/kitty_events.rb
+++ lib/kitty_events.rb
@@ -13,17 +13,14 @@ require 'active_job'
 # An event handler is just a ActiveJob worker that implements .perform(event, object).
 # When an event is triggered, It will fan out to all subscribers via ActiveJob
 module KittyEvents
-  @@events = []
   @@handlers = {}
 
-  mattr_reader :events, :handlers
+  mattr_reader :handlers
 
   def self.register(*event_names)
-    event_names.each do |event|
-      events << event.to_sym
+    event_names.each do |name|
+      handlers[name.to_sym] ||= []
     end
-
-    events.uniq!
   end
 
   def self.subscribe(event, handler)
@@ -39,13 +36,17 @@ module KittyEvents
     KittyEvents::HandleWorker.perform_later(event.to_s, object)
   end
 
+  def self.events
+    handlers.keys
+  end
+
   def self.handle(event, object)
     (handlers[event.to_sym] || []).each do |handler|
       handler.perform_later(event, object)
     end
   end
 
-  private
+  private_class_method
 
   def self.ensure_registered_event(event)
     return if events.include? event
