936914f993bd752bc194bf869e5734bc6c729586
Radoslav Stankov <rstankov@gmail.com>
2016-12-26 17:52:17 +0200

    Have proper private module methods

--- lib/kitty_events.rb
+++ lib/kitty_events.rb
@@ -17,38 +17,42 @@ module KittyEvents
 
   mattr_reader :handlers
 
-  def self.register(*event_names)
-    event_names.each do |name|
-      handlers[name.to_sym] ||= []
+  class << self
+    def register(*event_names)
+      event_names.each do |name|
+        handlers[name.to_sym] ||= []
+      end
     end
-  end
 
-  def self.subscribe(event, handler)
-    handlers = handlers_for_event! event
-    handlers << handler
-  end
-
-  def self.trigger(event, object)
-    handlers_for_event! event
-
-    KittyEvents::HandleWorker.perform_later(event.to_s, object)
-  end
-
-  def self.events
-    handlers.keys
-  end
-
-  def self.handle(event, object)
-    handlers_for_event(event) { [] }.each do |handler|
-      handler.perform_later(object)
+    def subscribe(event, handler)
+      handlers = handlers_for_event! event
+      handlers << handler
     end
-  end
 
-  def self.handlers_for_event!(event)
-    handlers_for_event(event) { raise ArgumentError, "#{event} is not registered" }
-  end
+    def trigger(event, object)
+      handlers_for_event! event
 
-  def self.handlers_for_event(event, &block)
-    handlers.fetch(event.to_sym, &block);
+      KittyEvents::HandleWorker.perform_later(event.to_s, object)
+    end
+
+    def events
+      handlers.keys
+    end
+
+    def handle(event, object)
+      handlers_for_event(event) { [] }.each do |handler|
+        handler.perform_later(object)
+      end
+    end
+
+    private
+
+    def handlers_for_event!(event)
+      handlers_for_event(event) { raise ArgumentError, "#{event} is not registered" }
+    end
+
+    def handlers_for_event(event, &block)
+      handlers.fetch(event.to_sym, &block);
+    end
   end
 end
